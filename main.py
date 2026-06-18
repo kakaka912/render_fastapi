4e6aba3a83fff8172eb4749c20396468ae669ec9
kakaka912 <delaware20050912@gmail.com>
2026-06-18 13:56:35 +0900

    fast

--- main.py
+++ main.py
@@ -22,13 +22,13 @@ def index():
         </head>
         <body style="background-color:#eef; text-align:center; padding-top:50px; font-family:sans-serif;">
             <h1 style="color:#333;">間違い探しを始めるよ</h1>
-            <p>束束束束束束束束束束束束束束束束束束束束束束束束束束束束束束</p>
+            <p style="cursor: pointer;">束束束束束束束束束束束束束束束束束束束束束束束束束束束束束束</p>
             <p>束束束束束束束束束束束束束束束束束束束束束束束束束束束束束束</p>
             <p>束束束束束束束束束束束束束束束束束束束束束束束束束束束束束束</p>
             <p>束束束束束束束束束束束束束束束束束束束束束束束束束束束束束束</p>
             <p>束束束束束束束束束束束束束束束束束束束束束束束束束束束束束束</p>
             <!-- 正解） -->
-            <p style="font-size: 24px; cursor: pointer;" onclick="correct()">束束<b>東</b>束束束束束束束束束束束束束束束束束束束束束束束束束束束</p>
+            <p>束束<body style="background-color:#eef; text-align:center; padding-top:50px; font-family:sans-serif; cursor: pointer;" onclick="correct()">東</b><body style="background-color:#eef; text-align:center; padding-top:50px; font-family:sans-serif;">束束束束束束束束束束束束束束束束束束束束束束束束束束束</p>
             <p>束束束束束束束束束束束束束束束束束束束束束束束束束束束束束束</p>
             <hr style="margin:40px;">
             <p style="color:#666;">Powered by FastAPI × Render</p>
