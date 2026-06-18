87fc73505033fea59a9d959abab225aec716b57b
kakaka912 <delaware20050912@gmail.com>
2026-06-18 13:43:43 +0900

    complete

--- main.py
+++ main.py
@@ -13,7 +13,12 @@ def index():
     <html>
         <head>
             <meta charset="UTF-8">
-            <title>間違い探し</title>
+            <title>間違い探しぢゃよ</title>
+            <script>
+                function correct() {
+                    alert("正解！");
+                }
+            </script>
         </head>
         <body style="background-color:#eef; text-align:center; padding-top:50px; font-family:sans-serif;">
             <h1 style="color:#333;">間違い探しを始めるよ</h1>
@@ -22,7 +27,8 @@ def index():
             <p>束束束束束束束束束束束束束束束束束束束束束束束束束束束束束束</p>
             <p>束束束束束束束束束束束束束束束束束束束束束束束束束束束束束束</p>
             <p>束束束束束束束束束束束束束束束束束束束束束束束束束束束束束束</p>
-            <p>束束東束束束束束束束束束束束束束束束束束束束束束束束束束束束</p>
+            <!-- 正解） -->
+            <p style="font-size: 24px; cursor: pointer;" onclick="correct()">束束<b>東</b>束束束束束束束束束束束束束束束束束束束束束束束束束束束</p>
             <p>束束束束束束束束束束束束束束束束束束束束束束束束束束束束束束</p>
             <hr style="margin:40px;">
             <p style="color:#666;">Powered by FastAPI × Render</p>
